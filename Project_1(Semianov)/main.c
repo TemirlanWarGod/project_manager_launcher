e8fefb2bd71fa74093080ab585e390e8be83caa6
kuruto012 <a.a.semianov@gmail.com>
2026-03-20 15:42:09 +0700

    Update main.c

--- Project_1(Semianov)/main.c
+++ Project_1(Semianov)/main.c
@@ -8,14 +8,14 @@ int ret_int(char *s) {
 	return strtol(s, &endptr, 10);
 }
 void git() {
-	printf("ввидите слагаемые(в конце ввидите end)\n");
+	printf("enter the terms(Write 'end' when you're done)\n");
 	char* vvod[10000];
 	fgets(vvod, 10000, stdin);
 	int prin = 0;
 	for (; strcmp(vvod, "end\n") != 0; fgets(vvod, 10000, stdin)) {
 		prin += ret_int(vvod);
 	}
-	printf("Сумма равна %d", prin);
+	printf(the amount is equal to "%d", prin);
 }
 int main(void) {
 	setlocale(LC_ALL, "");
